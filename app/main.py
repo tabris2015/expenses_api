325592f9e4bafbf4730455ac89f85d2de9908536
Jose Laruta <jose.laruta@coxautoinc.com>
2021-09-12 01:40:48 -0400

    added reload param to main

--- app/main.py
+++ app/main.py
@@ -6,5 +6,4 @@ app = FastAPI(title="basic_api")
 app.include_router(api_router, prefix="/v1")
 
 if __name__ == '__main__':
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
-
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
