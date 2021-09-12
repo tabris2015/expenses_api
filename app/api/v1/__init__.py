dd90ea474135a1e20265fc81185afe213d373637
Jose Laruta <jose.laruta@coxautoinc.com>
2021-09-12 01:19:32 -0400

    added expense api

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1 import products
+from app.api.v1 import products, expenses
 
 api_router = APIRouter()
 api_router.include_router(products.router, prefix="/products", tags=["products"])
+api_router.include_router(expenses.router, prefix="/expenses", tags=["expenses"])
---
+++ app/api/v1/expenses.py
@@ -0,0 +1,47 @@
+from typing import Any, List
+from fastapi import APIRouter, Depends, HTTPException, status, Query
+from sqlmodel import Session, select
+from app.models.expense import Expense, ExpenseCreate, ExpenseUpdate
+from app.database.session import get_session
+from app import crud, schemas
+
+
+router = APIRouter()
+
+
+@router.get("", response_model=List[Expense])
+def read_expenses(db: Session = Depends(get_session), skip: int = 0, limit: int = 100):
+    expenses = crud.expense.get_multi(db, skip=skip, limit=limit)
+    return expenses
+
+
+@router.get("/{id}", response_model=Expense)
+def read_expense(*, db: Session = Depends(get_session), id: int):
+    expense = crud.expense.get(db, model_id=id)
+    if not expense:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense does not exist")
+    return expense
+
+
+@router.post("", response_model=Expense)
+def create_expense(*, db: Session = Depends(get_session), expense_in: ExpenseCreate):
+    expense = crud.expense.create(db, obj_in=expense_in)
+    return expense
+
+
+@router.put("", response_model=Expense)
+def update_expense(*, db: Session = Depends(get_session), expense_in: ExpenseUpdate):
+    expense = crud.expense.get(db, model_id=expense_in.id)
+    if not expense:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense does not exist")
+    expense = crud.expense.update(db, db_obj=expense, obj_in=expense_in)
+    return expense
+
+
+@router.delete("", response_model=schemas.Message)
+def delete_expense(*, db: Session = Depends(get_session), id: int):
+    expense = crud.expense.get(db, model_id=id)
+    if not expense:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Expense does not exist")
+    crud.expense.remove(db, model_id=expense.id)
+    return {"message": f"Expense with ID={id} deleted."}
