589e474bd7c6a2e43fd6a98ec27e4111df3ea6be
Jose Laruta <jose.laruta@coxautoinc.com>
2021-09-14 17:42:04 -0400

    added category_id field to expense model

--- app/models/expense.py
+++ app/models/expense.py
@@ -1,10 +1,12 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
+from app.models.category import Category
 
 
 class ExpenseBase(SQLModel):
     description: Optional[str]
     amount: Optional[int]
+    category_id: Optional[int] = Field(default=None, foreign_key="category.id")
 
 
 class Expense(ExpenseBase, table=True):
