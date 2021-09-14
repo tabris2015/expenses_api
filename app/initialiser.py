bb9caaebe547647285cf53e31db499793c0c26d1
Jose Laruta <jose.laruta@coxautoinc.com>
2021-09-14 17:41:19 -0400

    added category model to db creation

--- app/initialiser.py
+++ app/initialiser.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel
 from app.database.session import engine
 from app.models.product import Product
 from app.models.expense import Expense
+from app.models.category import Category
 
 
 def create_db_and_tables():
